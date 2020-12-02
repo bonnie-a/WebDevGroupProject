5367a0335e403829c696a807b55e804e9d212ca5
slee293 <60554827+slee293@users.noreply.github.com>
2020-12-02 14:49:14 -0500

    Update fifteen.js
    Added start, end function to keep track of the time and moves

--- fifteen.js
+++ fifteen.js
@@ -1,4 +1,21 @@
+var startTime, endTime;
+var move = 0;
+var bgm = new Audio("bgm.mp3");
+
+function startGame() {
+	bgm.play();
+	startTime = performance.now();
+}
+
+function endGame() {
+	endTime = performance.now();
+	var timeScore = Math.round((endTime - startTime)/1000);
+	document.getElementById("time").innerHTML = timeScore + " seconds";
+	document.getElementById("move").innerHTML = move + " moves";
+}
+
 function select() {
+    move++;
     var num = this.innerHTML;
     var pos = this.style.backgroundPosition;
     var id = this.id;
