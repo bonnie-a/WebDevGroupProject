ffa3f839ff34c9c82ee78a21eed8454ba77647da
zol32 <70735687+zol32@users.noreply.github.com>
2020-12-02 19:43:13 -0500

    Add files via upload

--- fifteen.js
+++ fifteen.js
@@ -66,6 +66,50 @@ function init(){
     POSITIONS = Array.from(POSITIONS);
 }
 
+function shuffler(){
+    tiles = document.getElementsByClassName('tile');
+    var pos = 15;
+    var squares = [];
+    var random;
+    var num;
+    var test = false;
+
+    for(var i = 0; i < 2000; i++){
+        squares [0] = pos - 1;
+        squares [1] = pos - 4;
+        squares [2] = pos + 1;
+        squares [3] = pos + 4;
+        test = false;
+        
+        while(test === false){
+            random = Math.floor(Math.random() * 4); 
+            if(typeof tiles[random] != 'undefined'){
+                num = squares[random];
+                swapEmpty(tiles[num]);
+                pos = squares[random];
+                test = true;
+            }
+        }
+        
+    }
+}
+
+function swapEmpty(tile){
+    var num = tile.innerHTML;
+    var pos = tile.style.backgroundPosition;
+    var id = tile.id;
+    
+    document.getElementById("empty").style.backgroundPosition = pos;
+    document.getElementById("empty").innerHTML = num;
+    document.getElementById("empty").className = "tile";
+    document.getElementById("empty").id = id;
+    
+    
+    tile.innerHTML = "";
+    tile.id = "empty";
+    tile.className = "empty";
+}
+
 var startTime, endTime;
 var moves = 0;
 var bgm = new Audio("bgm.mp3");
